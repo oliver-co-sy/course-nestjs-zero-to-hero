7f5db564f27f887131faca5c6d85ba00e01966b3
Oliver Sy <oliver@veritas.local>
2024-01-23 22:09:53 -0500

    updated

--- nestjs-task-management/src/tasks/dto/get-tasks-filter.dto.ts
+++ nestjs-task-management/src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
 import { TaskStatus } from "../tasks.model";
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class GetTasksFilterDto {
+
+    @IsEnum(TaskStatus)
+    @IsOptional()
     status?: TaskStatus;
+
+    @IsOptional()
+    @IsString()
     search?: string;
 }
